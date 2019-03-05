208fdee56eb0d0afcffa3db175612731e6e4a279
Jakub Konka <kubkon@jakubkonka.com>
2019-03-05 08:07:13 +0100

    Add dirs and files visitor (breadth-first)

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -1,8 +1,6 @@
 mod error_info;
-mod virtual_fs;
 
 use error_info::report_pending_exception;
-use virtual_fs::VirtualFS;
 
 use mozjs::glue::SetBuildId;
 use mozjs::jsapi::BuildIdCharVector;
@@ -23,19 +21,26 @@ use mozjs::jsval::UndefinedValue;
 use mozjs::rust::{JSEngine, Runtime, SIMPLE_GLOBAL_CLASS};
 use mozjs::typedarray::{ArrayBuffer, CreateWith};
 
+use std::collections::{HashMap, VecDeque};
 use std::ffi::CStr;
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 use std::ptr;
 use std::str;
 
 pub struct Sandbox {
     runtime: Runtime,
     global: *mut JSObject,
+    vfs: zbox::Repo,
 }
 
 impl Sandbox {
-    pub fn new(input_dir: &str, output_dir: &str) -> Self {
+    const SANDBOX_PATH: &'static str = "sandbox";
+    const SANDBOX_PWD: &'static str = "$andb0x_g0l3m_w@sm";
+
+    pub fn new() -> Self {
         let engine =
             JSEngine::init().unwrap_or_else(|err| panic!("Error initializing JSEngine: {:?}", err));
         let runtime = Runtime::new(engine);
@@ -53,7 +58,22 @@ impl Sandbox {
             )
         };
 
-        let sandbox = Sandbox { runtime, global };
+        zbox::init_env();
+        let vfs = zbox::RepoOpener::new()
+            .create_new(true)
+            .open(&format!("file://{}", Self::SANDBOX_PATH), Self::SANDBOX_PWD)
+            .unwrap_or_else(|err| {
+                panic!(
+                    "Error initializing VirtualFS: repo already exists: {:?}",
+                    err
+                )
+            });
+
+        let sandbox = Sandbox {
+            runtime,
+            global,
+            vfs,
+        };
         sandbox.init();
 
         sandbox
@@ -89,9 +109,44 @@ impl Sandbox {
         self.evaluate_script("var Module = {'printErr': print, 'print': print};");
     }
 
-    pub fn map_dir(&mut self, input_dir: &str) {}
+    pub fn map_input_dir(&mut self, input_dir: &str) {
+        let input_path = Path::new(input_dir);
+        let rel_input_path = input_path
+            .file_name()
+            .and_then(std::ffi::OsStr::to_str)
+            .unwrap_or_else(|| panic!("invalid filename!"));
+        let mut rel_path = PathBuf::new();
+        rel_path.push("/");
+        rel_path.push(rel_input_path);
 
-    pub fn add_output_file(&mut self, output_file: &str) {}
+        let mut abs_path = PathBuf::new();
+        abs_path.push(input_path);
+        let path = PathCombo(abs_path.into_boxed_path(), rel_path.into_boxed_path());
+
+        visit(path, &mut |path: &PathCombo| {
+            println!("{:?} => {:?}", path.0, path.1);
+
+            if path.0.is_dir() {
+                self.vfs.create_dir(&path.1).unwrap();
+            } else {
+                let mut file = self.vfs.create_file(&path.1).unwrap();
+                // write contents over
+                let contents = read_file(&path.0).unwrap();
+                file.write_once(&contents).unwrap();
+            }
+        })
+        .unwrap_or_else(|err| {
+            panic!("couldn't map input files into MemFS: {:?}", err);
+        });
+
+        rvisit(&self.vfs, Path::new("/"), &|entry| {
+            let path = entry.path();
+            println!("{:?}", path);
+        })
+        .unwrap();
+    }
+
+    pub fn map_output_dir(&mut self, output_dir: &str, output_file: &str) {}
 
     pub fn run(&self, wasm_js: &str, wasm_bin: &str) {
         self.evaluate_script("print(1);")
@@ -109,6 +164,74 @@ impl Sandbox {
                 report_pending_exception(ctx, true);
             });
     }
+}
+
+struct PathCombo(Box<Path>, Box<Path>);
+
+fn rvisit(vfs: &zbox::Repo, path: &Path, cb: &Fn(&zbox::DirEntry)) -> zbox::Result<()> {
+    println!("{:?}", path);
+
+    if vfs.is_dir(path) {
+        for entry in vfs.read_dir(path)? {
+            let path = entry.path();
+
+            if vfs.is_dir(path) {
+                rvisit(vfs, &path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn visit(path: PathCombo, cb: &mut FnMut(&PathCombo)) -> std::io::Result<()> {
+    let mut fifo = VecDeque::new();
+    fifo.push_back(path);
+
+    while let Some(path) = fifo.pop_front() {
+        cb(&path);
+        let PathCombo(abs_path, rel_path) = path;
+
+        if abs_path.is_dir() {
+            for entry in fs::read_dir(abs_path)? {
+                let entry = entry?;
+                let abs_path = entry.path();
+
+                let rel_input_path = abs_path
+                    .file_name()
+                    .and_then(std::ffi::OsStr::to_str)
+                    .unwrap_or_else(|| panic!("invalid filename!"));
+                let mut rel_path = rel_path.clone().into_path_buf();
+                rel_path.push(rel_input_path);
+                let path = PathCombo(abs_path.into_boxed_path(), rel_path.into_boxed_path());
+                fifo.push_back(path);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+impl Drop for Sandbox {
+    fn drop(&mut self) {
+        // remove repo if was created successfully
+        if let Ok(res) = zbox::Repo::exists(&format!("file://{}", Self::SANDBOX_PATH)) {
+            if res {
+                std::fs::remove_dir_all(Self::SANDBOX_PATH)
+                    .unwrap_or_else(|err| log::error!("Sandbox VirtualFS didn't exist: {:?}", err));
+            }
+        }
+    }
+}
+
+fn read_file<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<u8>> {
+    let mut file = File::open(path)?;
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)?;
+
+    Ok(contents)
 }
 
 unsafe extern "C" fn sp_build_id(build_id: *mut BuildIdCharVector) -> bool {
