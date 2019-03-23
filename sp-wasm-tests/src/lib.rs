8c34e959f4386db3b5a988d48e2b08421916067f
Jakub Konka <kubkon@jakubkonka.com>
2019-03-23 10:05:29 +0100

    Fix security vulnerability allowing user to escape the sandbox

--- sp-wasm-tests/src/lib.rs
+++ sp-wasm-tests/src/lib.rs
@@ -1,7 +1,13 @@
 use std::env;
-use std::path;
 use std::ffi;
+use std::path;
 
 pub fn current_prog_name() -> Option<String> {
-    env::args().next().as_ref().map(path::Path::new).and_then(path::Path::file_name).and_then(ffi::OsStr::to_str).map(String::from)
+    env::args()
+        .next()
+        .as_ref()
+        .map(path::Path::new)
+        .and_then(path::Path::file_name)
+        .and_then(ffi::OsStr::to_str)
+        .map(String::from)
 }
