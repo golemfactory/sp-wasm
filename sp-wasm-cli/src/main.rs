485a95d66d2c1f2a778a9b5977ee982396061962
Jakub Konka <jakub.konka@golem.network>
2019-06-27 12:35:15 +0200

    Switch from docopt to clap

--- sp-wasm-cli/src/main.rs
+++ sp-wasm-cli/src/main.rs
@@ -1,50 +1,74 @@
 #![warn(clippy::all)]
 
+use clap::{value_t_or_exit, values_t_or_exit, App, Arg};
 use sp_wasm_engine::prelude::*;
 
-use docopt::Docopt;
-use serde::Deserialize;
-
-const USAGE: &str = "
-Standalone SpiderMonkey instance that can be used to run any Emscripten
-generated WASM according to the Golem calling convention.
-
-Usage:
-    wasm-sandbox -I <input-dir> -O <output-dir> -j <wasm-js> -w <wasm> -o <output-file>... [-- <args>...]
-    wasm-sandbox (-h | --help)
-
-Options:
-    -I <input-dir>          Path to the input dir.
-    -O <output-dir>         Path to the output dir.
-    -j <wasm-js>            Path to the Emscripten JavaScript glue code.
-    -w <wasm-bin>           Path to the Emscripten WASM binary.
-    -o <output-file>        Path to expected file.
-    <args>                  WASM program arguments. 
-    -h --help               Show this screen.
-";
-
-#[allow(non_snake_case)]
-#[derive(Debug, Deserialize)]
-struct Args {
-    flag_I: String,
-    flag_O: String,
-    flag_j: String,
-    flag_w: String,
-    flag_o: Vec<String>,
-    arg_args: Vec<String>,
-}
-
 fn main() {
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|dopt| dopt.deserialize())
-        .unwrap_or_else(|e| e.exit());
+    let args = App::new("wasm-sandbox")
+        .version("0.2.1")
+        .author("Golem RnD Team <contact@golem.network>")
+        .about("Standalone SpiderMonkey instance that can be used to run Emscripten generated Wasm according to the Golem calling convention.")
+        .arg(
+            Arg::with_name("input_dir")
+                .short("I")
+                .value_name("input-dir")
+                .required(true)
+                .takes_value(true)
+                .help("Path to input dir"),
+        )
+        .arg(
+            Arg::with_name("output_dir")
+                .short("O")
+                .value_name("output-dir")
+                .required(true)
+                .takes_value(true)
+                .help("Path to output dir"),
+        )
+        .arg(
+            Arg::with_name("wasm_js")
+                .short("j")
+                .value_name("wasm-js")
+                .required(true)
+                .takes_value(true)
+                .help("Path to Emscripten JavaScript glue code"),
+        )
+        .arg(
+            Arg::with_name("wasm_bin")
+                .short("w")
+                .value_name("wasm-bin")
+                .required(true)
+                .takes_value(true)
+                .help("Path to Emscripten Wasm binary"),
+        )
+        .arg(
+            Arg::with_name("output_file")
+                .short("o")
+                .value_name("output-file")
+                .required(true)
+                .multiple(true)
+                .help("Path to expected output file"),
+        )
+        .arg(
+            Arg::with_name("wasm_args")
+                .value_name("wasm-args")
+                .multiple(true)
+                .help("Wasm program args"),
+        )
+        .get_matches();
 
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
+    let input_dir = value_t_or_exit!(args.value_of("input_dir"), String);
+    let output_dir = value_t_or_exit!(args.value_of("output_dir"), String);
+    let wasm_js = value_t_or_exit!(args.value_of("wasm_js"), String);
+    let wasm_bin = value_t_or_exit!(args.value_of("wasm_bin"), String);
+    let output_files = values_t_or_exit!(args.values_of("output_file"), String);
+    let wasm_args = values_t_or_exit!(args.values_of("wasm_args"), String);
+
     Sandbox::new()
-        .and_then(|sandbox| sandbox.set_exec_args(args.arg_args.iter()))
-        .and_then(|sandbox| sandbox.load_input_files(&args.flag_I))
-        .and_then(|sandbox| sandbox.run(&args.flag_j, &args.flag_w))
-        .and_then(|sandbox| sandbox.save_output_files(&args.flag_O, args.flag_o.iter()))
+        .and_then(|sandbox| sandbox.set_exec_args(wasm_args.iter()))
+        .and_then(|sandbox| sandbox.load_input_files(input_dir))
+        .and_then(|sandbox| sandbox.run(wasm_js, wasm_bin))
+        .and_then(|sandbox| sandbox.save_output_files(output_dir, output_files.iter()))
         .unwrap_or_else(|err| eprintln!("{}", err));
 }
